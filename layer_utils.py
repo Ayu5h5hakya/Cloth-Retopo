031d8982088bab1821d0e654562d985d791c99db
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2023-02-22 16:51:28 +0545

    add field to create name of the layer

--- layer_utils.py
+++ layer_utils.py
@@ -1,9 +1,14 @@
 import maya.cmds as mc
 
+global layerNameField
+
 def showUI():
     window = mc.window(title="Layer utilities", widthHeight=(400,400))
     mc.columnLayout()
     mc.text(label="Collection of shortcut for layer related tasks")
+    mc.text(label="Layer name")
+    layerNameField = mc.textField() 
+
     mc.showWindow(window)
 
 showUI()
