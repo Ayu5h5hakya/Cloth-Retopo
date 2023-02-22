28827eaf2748d35100efa1e172b6ab7afb071124
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2023-02-22 16:54:48 +0545

    add button to create layer

--- layer_utils.py
+++ layer_utils.py
@@ -2,6 +2,9 @@ import maya.cmds as mc
 
 global layerNameField
 
+def createLayer(args):
+    print('hello')
+
 def showUI():
     window = mc.window(title="Layer utilities", widthHeight=(400,400))
     mc.columnLayout()
@@ -9,6 +12,7 @@ def showUI():
     mc.text(label="Layer name")
     layerNameField = mc.textField() 
 
+    mc.button(label="Create", command = createLayer, enable = True) 
     mc.showWindow(window)
 
 showUI()
