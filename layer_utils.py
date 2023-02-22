ab2122e359f6912ee6e676d0bbc3349c24463b23
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2023-02-22 17:37:28 +0545

    add childern to layer flag

--- layer_utils.py
+++ layer_utils.py
@@ -1,18 +1,24 @@
 import maya.cmds as mc
 
 global layerNameField
+global noRecurseField
 
 def createLayer(args):
-    layerName = mc.textField(layerNameField, query=True, value=True)
-    mc.createDisplayLayer(noRecurse=True, name=layerName)
+    global layerNameField
+    global noRecurseField
+    layerName = mc.textField(layerNameField, query=True, text=True)
+    noRecurseFlag = mc.checkBox(noRecurseField, query = True, value=True)
+    mc.createDisplayLayer(noRecurse=noRecurseFlag, name=layerName)
 
 def showUI():
+    global layerNameField
+    global noRecurseField
     window = mc.window(title="Layer utilities", widthHeight=(400,400))
     mc.columnLayout()
     mc.text(label="Collection of shortcut for layer related tasks")
     mc.text(label="Layer name")
     layerNameField = mc.textField() 
-
+    noRecurseField = mc.checkBox( label='Add children to layer' )
     mc.button(label="Create", command = createLayer, enable = True) 
     mc.showWindow(window)
 
